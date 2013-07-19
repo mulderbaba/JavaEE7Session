8c0897bacd96ea9146c3ab693c8c963ac3aaefed
Mert ÇALIŞKAN <mert@t2.com.tr>
2013-07-19 19:53:04 +0300

    initial

---
+++ JSF22/src/main/java/com/primetek/javaee7/jsf22/html5/FriendlyMarkupBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.primetek.javaee7.jsf22.html5;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author mertcaliskan
+ */
+@Named(value = "friendlyMarkupBean")
+@Dependent
+public class FriendlyMarkupBean {
+
+    /**
+     * Creates a new instance of FriendlyMarkupBean
+     */
+    public FriendlyMarkupBean() {
+    }
+}
